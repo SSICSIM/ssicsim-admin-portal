ac5eaed702589ba5c16b95a373083e460bb68dcb
Nuthanan Tharmarajah <n2tharma@uwaterloo.ca>
2026-04-03 15:10:21 -0400

    added SEO optim for admin portal

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,10 +1,24 @@
-import { withAuth } from "next-auth/middleware";
+import { withAuth, type NextRequestWithAuth } from "next-auth/middleware";
+import { NextResponse, type NextFetchEvent } from "next/server";
 
-export default withAuth({
+const authMiddleware = withAuth({
   pages: {
     signIn: "/sign-in"
   }
 });
+
+export default function middleware(
+  req: NextRequestWithAuth,
+  event: NextFetchEvent
+) {
+  const response = authMiddleware(req, event);
+
+  if (response instanceof NextResponse) {
+    response.headers.set("X-Robots-Tag", "noindex, nofollow");
+  }
+
+  return response;
+}
 
 export const config = {
   matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|sign-in).*)"]
