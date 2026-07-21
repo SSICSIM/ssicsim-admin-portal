48245c11711e79deb2f804432635f5575c4ba9f1
Nuthanan Tharmarajah <nuthanan06@gmail.com>
2026-07-21 15:51:30 -0400

    fixed ruff

--- backend/migrations/versions/7f9b9c128399_sec_member_edits.py
+++ backend/migrations/versions/7f9b9c128399_sec_member_edits.py
@@ -8,9 +8,8 @@ Create Date: 2026-07-20 19:39:12.111016
 
 from __future__ import annotations
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '7f9b9c128399'
