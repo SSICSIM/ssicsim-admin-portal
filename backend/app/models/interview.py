adce76e68866e6054026b3c5d0d4c09d37d2d127
Elizabeth Wright <e3wright@uwaterloo.ca>
2026-07-26 15:44:09 -0400

    interview model updates and migration

--- backend/app/models/interview.py
+++ backend/app/models/interview.py
@@ -18,14 +18,14 @@ class Interview(Base):
         UUID(as_uuid=True), ForeignKey("applicants.id", ondelete="SET NULL"), primary_key=True
     )
     sec_member_id1: Mapped[uuid.UUID | None] = mapped_column(
-        UUID(as_uuid=True), ForeignKey("sec_member.id", ondelete="SET NULL")
+        UUID(as_uuid=True), ForeignKey("sec_members.id", ondelete="SET NULL")
     )
     sec_member_id2: Mapped[uuid.UUID | None] = mapped_column(
-        UUID(as_uuid=True), ForeignKey("sec_member.id", ondelete="SET NULL")
+        UUID(as_uuid=True), ForeignKey("sec_members.id", ondelete="SET NULL")
     )
     
-    isConfirmed1: Mapped[bool | None] = mapped_column(Boolean)
-    isConfirmed2: Mapped[bool | None] = mapped_column(Boolean)
+    isConfirmed1: Mapped[bool | None] = mapped_column(Boolean, default=False)
+    isConfirmed2: Mapped[bool | None] = mapped_column(Boolean, default=False)
 
     interviewDateTime: Mapped[datetime | None] = mapped_column(
         DateTime(timezone=True)
