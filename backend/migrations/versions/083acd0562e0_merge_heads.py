6d0ffe0d62966df4c6a02507cd4a5688c8772992
Elizabeth Wright <e3wright@uwaterloo.ca>
2026-08-24 19:22:21 -0400

    merge migrations

---
+++ backend/migrations/versions/083acd0562e0_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge_heads
+
+Revision ID: 083acd0562e0
+Revises: 1107e57c1b83, 3fc38ecd1085
+Create Date: 2026-08-24 23:21:19.990650
+
+"""
+
+from __future__ import annotations
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '083acd0562e0'
+down_revision = ('1107e57c1b83', '3fc38ecd1085')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
+
